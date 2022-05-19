202de9d1e4578f808f51df00c50657337038dda0
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-19 14:49:07 +0200

    AppEvents

---
+++ src/components/AppEvents.vue
@@ -0,0 +1,55 @@
+<template>
+    <section >
+        <h2 class="text-center">Upcoming Events</h2>
+        <p class="text-center">MEET THE AUTHOR IN PERSON</p>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="../assets/img/event-05.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="../assets/img/event-04.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="../assets/img/event-07.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+        </div>
+        
+    </section>
+</template>
+
+<script>
+export default {
+    nome: 'AppEvents',
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+section{
+    background-color: #FEF7E9;
+    padding-bottom: 50px;
+}
+
+</style>
