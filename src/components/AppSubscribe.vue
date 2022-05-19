61972edebd67b465d41a3a2316498f4205e0a62c
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-19 14:51:44 +0200

    AppSubsribe

---
+++ src/components/AppSubscribe.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="text-center">
+        <h2>Subscribe To My <br> Newsletter</h2>
+        <p>BE NOTIFIED ABOUT BOOK SIGNING TOUR DATES</p>
+        <input type="search" placeholder="Insert your email...*" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+        <p>Donec fringilla nunc eu turpis dignissim, at euismod sapien tincidunt.</p>
+
+    </section>
+</template>
+
+<script>
+export default {
+    nome: 'AppSubscribe',
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+section{
+    background-image: url(../assets/img/banner-2.jpg);
+    background-size: cover;
+    background-position: center;
+    color: white;
+    padding: 100px;
+}
+
+</style>
